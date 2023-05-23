c8c993d6aa6ce9e2dd279f3a94944ebcb9b43ae1
Kseniya Kavalenka <kavalenkakseniya@gmail.com>
2023-05-23 11:14:10 +0200

    Add horizontal resizer

--- codeocean/app/assets/javascripts/editor.js
+++ codeocean/app/assets/javascripts/editor.js
@@ -19,5 +19,27 @@ $(document).on('turbolinks:load', function(event) {
     // It must not be called during page navigation. Otherwise, content will be duplicated!
     // Search for insertLines and Turbolinks reload / cache control
     CodeOceanEditor.initializeEverything();
+  };
+
+  let ismdwn = 0
+  rpanrResize.addEventListener('mousedown', mD)
+
+  function mD(event) {
+    ismdwn = 1
+    document.body.addEventListener('mousemove', mV)
+    document.body.addEventListener('mouseup', end)
+  }
+
+  function mV(event) {
+    if (ismdwn === 1) {
+      pan1.style.flexBasis = event.clientX + "px"
+    } else {
+      end()
+    }
+  }
+  const end = (e) => {
+    ismdwn = 0
+    document.body.removeEventListener('mouseup', end)
+    rpanrResize.removeEventListener('mousemove', mV)
   }
 });
