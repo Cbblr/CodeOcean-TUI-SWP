e9e868416ba91bc92771c541e23894e92feb0580
Maximilian Weis <maximilian.weis@tu-ilmenau.de>
2023-07-04 19:52:08 +0000

    Delete 20230623092557_rename_user_exercise_feedback_columns.rb

--- codeocean/db/migrate/20230623092557_rename_user_exercise_feedback_columns.rb
+++
@@ -1,6 +0,0 @@
-class RenameUserExerciseFeedbackColumns < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :user_exercise_feedbacks, :user_error_feedback_minutes, :user_estimated_worktime_minutes
-    rename_column :user_exercise_feedbacks, :user_error_feedback_hours, :user_estimated_worktime_hours
-  end
-end
